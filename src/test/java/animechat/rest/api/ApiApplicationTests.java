bf516e402855d3fab3c013a285718a8a611cceac
BeauTaapken <beautaapken@me.com>
2020-01-04 11:43:01 +0100

    * Added tests for wrong outcome

--- src/test/java/animechat/rest/api/ApiApplicationTests.java
+++ src/test/java/animechat/rest/api/ApiApplicationTests.java
@@ -46,111 +46,177 @@ public class ApiApplicationTests {
      * Code for testing the User model GetEmail
      */
     @Test
-    public void GetUserEmail(){
+    public void GetUserEmailCorrect(){
         Assert.assertEquals("test@test.com", user.getEmail());
+    }
+
+
+    @Test
+    public void GetUserEmailWrong(){
+        Assert.assertNotEquals("wrongTest@test.com", user.getEmail());
     }
 
     /**
      * Code for testing the User model SetEmail
      */
     @Test
-    public void SetUserEmail(){
+    public void SetUserEmailCorrect(){
         user.setEmail("newtest@test.com");
         Assert.assertEquals("newtest@test.com", user.getEmail());
     }
 
-    /**
-     * Code for testing the User model SetEmail
-     */
     @Test
     public void SetUserEmailWrong(){
-        user.setEmail("newtest");
-        Assert.assertEquals("newtest", user.getEmail());
+        user.setEmail("newwrongtest@test.com");
+        Assert.assertNotEquals("newtest@test.com", user.getEmail());
     }
 
     /**
      * Code for testing the User model GetName
      */
     @Test
-    public void GetUserName(){
+    public void GetUserNameCorrect(){
         Assert.assertEquals("testuser", user.getName());
+    }
+
+    @Test
+    public void GetUserNameWrong(){
+        Assert.assertNotEquals("testwronguser", user.getName());
     }
 
     /**
      * Code for testing the User model SetNAme
      */
     @Test
-    public void SetUserName(){
+    public void SetUserNameCorrect(){
         user.setName("newtestuser");
         Assert.assertEquals("newtestuser", user.getName());
+    }
+
+    @Test
+    public void SetUserNameWrong(){
+        user.setName("newwrongtestuser");
+        Assert.assertNotEquals("newtestuser", user.getName());
     }
 
     /**
      * Code for testing the User model GetImgUrl
      */
     @Test
-    public void GetImgUrl(){
+    public void GetImgUrlCorrect(){
         Assert.assertEquals("http://", user.getImgUrl());
+    }
+
+    @Test
+    public void GetImgUrlWrong(){
+        Assert.assertNotEquals("wronghttp://", user.getImgUrl());
     }
 
     /**
      * Code for testing the User model setImgUrl
      */
     @Test
-    public void SetImgUrl(){
+    public void SetImgUrlCorrect(){
         user.setImgUrl("newhttp://");
         Assert.assertEquals("newhttp://", user.getImgUrl());
+    }
+
+    @Test
+    public void SetImgUrlWrong(){
+        user.setImgUrl("newwronghttp://");
+        Assert.assertNotEquals("newhttp://", user.getImgUrl());
     }
 
     /**
      * Code for testing the Friend model GetId
      */
     @Test
-    public void GetFriendId(){
+    public void GetFriendIdCorrect(){
         Assert.assertEquals(1, friend.getId());
+    }
+
+    @Test
+    public void GetFriendIdWrong(){
+        Assert.assertNotEquals("wrong", friend.getId());
     }
 
     /**
      * Code for testing the Friend model SetId
      */
     @Test
-    public void SetFriendId(){
+    public void SetFriendIdCorrect(){
         friend.setId(8);
         Assert.assertEquals(8, friend.getId());
+    }
+
+    @Test
+    public void SetFriendIdWrong(){
+        friend.setId(8);
+        Assert.assertNotEquals("wrong", friend.getId());
     }
 
     /**
      * Code for testing the Friend model GetUserEmail
      */
     @Test
-    public void GetUserEmailFromFriend(){
+    public void GetUserEmailFromFriendCorrect(){
         Assert.assertEquals("test@test.com", friend.getUserEmail());
+    }
+
+    @Test
+    public void GetUserEmailFromFriendWrong(){
+        Assert.assertNotEquals("testwrong@test.com", friend.getUserEmail());
     }
 
     /**
      * Code for testing the Friend model SetUserEmail
      */
     @Test
-    public void SetUserEmailFromFriend(){
+    public void SetUserEmailFromFriendCorrect(){
         friend.setUserEmail("newtest@test.com");
         Assert.assertEquals("newtest@test.com", friend.getUserEmail());
+    }
+
+    @Test
+    public void SetUserEmailFromFriendWrong(){
+        friend.setUserEmail("newwrongtest@test.com");
+        Assert.assertNotEquals("newtest@test.com", friend.getUserEmail());
     }
 
     /**
      * Code for testing the Friend model GetFriendEmail
      */
     @Test
-    public void GetFriendEmail(){
-        Assert.assertEquals("testfriend@test.com", friend.getFriendEmail());
-    }
+    public void GetFriendEmailCorrect(){ Assert.assertEquals("testfriend@test.com", friend.getFriendEmail()); }
+
+    @Test
+    public void GetFriendEmailWrong(){ Assert.assertNotEquals("testwrongfriend@test.com", friend.getFriendEmail()); }
 
     /**
      * Code for testing the Friend model SetFriendEmail
      */
     @Test
-    public void SetFriendEmail(){
+    public void SetFriendEmailCorrect(){
         friend.setFriendEmail("newtestfriend@test.com");
         Assert.assertEquals("newtestfriend@test.com", friend.getFriendEmail());
+    }
+
+    @Test
+    public void SetFriendEmailWrong(){
+        friend.setFriendEmail("newwrongtestfriend@test.com");
+        Assert.assertNotEquals("newtestfriend@test.com", friend.getFriendEmail());
+    }
+
+    /**
+     * Code for testing if non existing url returns 404 status
+     */
+    @Test
+    public void Get404Error() throws Exception {
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get("/wrongurl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(user))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(404));
     }
 
     /**
